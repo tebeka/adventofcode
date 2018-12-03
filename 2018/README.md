6b4c0e4d92d6ebd005ab3aba7ea06da5463bba19
Miki Tebeka <miki.tebeka@gmail.com>
2018-12-03 14:02:37 +0200

    3/1 solved

--- 2018/README.md
+++ 2018/README.md
@@ -4,4 +4,4 @@ Solving [Advent of Code 2018](http://adventofcode.com/2018)
 
 * Day 1: [day1.clj](day1.clj)
 * Day 2: [day2.py](day2.py), [day2.clj](day2.clj)
-* Day 3: [day3.py](day3.py), [day3.go](day3.go) - NOT WORKING
+* Day 3: [day3.go](day3.go)
--- 2018/day3.go
+++ 2018/day3.go
@@ -1,4 +1,3 @@
-// FIXME: Wrong answer
 package main
 
 import (
@@ -16,73 +15,70 @@ var (
 	lineRe = regexp.MustCompile("([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)")
 )
 
-// Rect is rectangle
-type Rect struct {
-	//ID      int
-	TopX    int
-	TopY    int
-	BottomX int
-	BottomY int
+// Square on fabric
+type Square struct {
+	X int
+	Y int
 }
 
-func parseLine(line string) (*Rect, error) {
+// Claim is a claim on grid
+type Claim struct {
+	TopLeft     Square
+	BottomRight Square
+}
+
+func parseLine(line string) (*Claim, error) {
 	fields := lineRe.FindStringSubmatch(line)
 	if len(fields) != 6 {
 		return nil, fmt.Errorf("wrong number of fields - %d", len(fields))
 	}
 
-	var err error
-	r := &Rect{}
-	/*
-		r.ID, err = strconv.Atoi(fields[1])
-		if err != nil {
-			return nil, fmt.Errorf("bad id - %s", fields[1])
-		}
-	*/
-
-	r.TopX, err = strconv.Atoi(fields[2])
+	topX, err := strconv.Atoi(fields[2])
 	if err != nil {
 		return nil, fmt.Errorf("bad top x - %s", fields[2])
 	}
 
-	r.TopY, err = strconv.Atoi(fields[3])
+	topY, err := strconv.Atoi(fields[3])
 	if err != nil {
 		return nil, fmt.Errorf("bad top y - %s", fields[3])
 	}
 
-	r.BottomX, err = strconv.Atoi(fields[4])
+	width, err := strconv.Atoi(fields[4])
 	if err != nil {
 		return nil, fmt.Errorf("bad width - %s", fields[4])
 	}
-	r.BottomX += r.TopX
 
-	r.BottomY, err = strconv.Atoi(fields[5])
+	height, err := strconv.Atoi(fields[5])
 	if err != nil {
 		return nil, fmt.Errorf("bad height - %s", fields[5])
 	}
-	r.BottomY += r.TopY
 
-	return r, nil
+	claim := &Claim{
+		TopLeft:     Square{topX, topY},
+		BottomRight: Square{topX + width - 1, topY + height - 1},
+	}
+
+	return claim, nil
 }
 
-func parseFile(r io.Reader) ([]*Rect, error) {
-	var rects []*Rect
+func parseFile(r io.Reader) ([]*Claim, error) {
+	var claims []*Claim
 	lnum := 0
 	s := bufio.NewScanner(r)
 	for s.Scan() {
 		lnum++
-		r, err := parseLine(s.Text())
+		c, err := parseLine(s.Text())
 		if err != nil {
 			return nil, fmt.Errorf("%d: %s", lnum, err)
 		}
-		rects = append(rects, r)
+		claims = append(claims, c)
 	}
 
 	if err := s.Err(); err != nil {
 		return nil, err
 	}
 
-	return rects, nil
+	return claims, nil
 }
 
 func min(x, y int) int {
@@ -99,49 +95,50 @@ func max(x, y int) int {
 	return y
 }
 
-func inRect(r *Rect, x int, y int) bool {
-	if x < r.TopX || x > r.BottomX {
-		return false
+func overlap(c1, c2 *Claim) []Square {
+	minX := max(c1.TopLeft.X, c2.TopLeft.X)
+	maxX := min(c1.BottomRight.X, c2.BottomRight.X)
+	minY := max(c1.TopLeft.Y, c2.TopLeft.Y)
+	maxY := min(c1.BottomRight.Y, c2.BottomRight.Y)
+
+	var sqs []Square
+	for x := minX; x <= maxX; x++ {
+		for y := minY; y <= maxY; y++ {
+			sqs = append(sqs, Square{x, y})
+		}
 	}
 
-	if y < r.TopY || y > r.BottomY {
-		return false
-	}
-
-	return true
-}
-
-func overlap(r1, r2 *Rect) int {
-	// Max(0, Min(XA2, XB2) - Max(XA1, XB1))
-	// *
-	// Max(0, Min(YA2, YB2) - Max(YA1, YB1))
-	width := max(0, min(r1.BottomX, r2.BottomX)-max(r1.TopX, r2.TopX))
-	height := max(0, min(r1.BottomY, r2.BottomY)-max(r1.TopY, r2.TopY))
-	return width * height
+	return sqs
 }
 
 var data = `#1 @ 1,3: 4x4
 #2 @ 3,1: 4x4
 #3 @ 5,5: 2x2`
 
+func part1(claims []*Claim) {
+	dups := make(map[Square]bool)
+	for i, c1 := range claims {
+		for _, c2 := range claims[i+1:] {
+			for _, s := range overlap(c1, c2) {
+				dups[s] = true
+			}
+		}
+	}
+
+	fmt.Println(len(dups))
+}
+
 func main() {
 	file, err := os.Open("day-3.txt")
 	if err != nil {
 		panic(err)
 	}
-
-	//file := strings.NewReader(data)
-
-	rects, err := parseFile(file)
+	// file := strings.NewReader(data)
+	claims, err := parseFile(file)
 	if err != nil {
 		panic(err)
 	}
 
-	n := 0
-	for i, r1 := range rects {
-		for _, r2 := range rects[i+1:] {
-			n += overlap(r1, r2)
-		}
-	}
-	fmt.Println(n)
+	part1(claims)
+
 }
