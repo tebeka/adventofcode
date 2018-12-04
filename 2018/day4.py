f8eb2f2205f297b6e40001ed3f91d4f8492ff407
Miki Tebeka <miki.tebeka@gmail.com>
2018-12-04 14:34:53 +0200

    simpler (still wrong)

--- 2018/day4.py
+++ 2018/day4.py
@@ -2,78 +2,59 @@
 # [1518-11-01 00:05] falls asleep
 # [1518-11-01 00:25] wakes up
 
-from collections import Counter, namedtuple, defaultdict
-from datetime import datetime
-
-
-Record = namedtuple('Record', 'time id state')
-
-
-awake, asleep = 'awake', 'asleep'
-
-example = '''\
-[1518-11-01 00:00] Guard #10 begins shift
-[1518-11-01 00:05] falls asleep
-[1518-11-01 00:25] wakes up
-[1518-11-01 00:30] falls asleep
-[1518-11-01 00:55] wakes up
-[1518-11-01 23:58] Guard #99 begins shift
-[1518-11-02 00:40] falls asleep
-[1518-11-02 00:50] wakes up
-[1518-11-03 00:05] Guard #10 begins shift
-[1518-11-03 00:24] falls asleep
-[1518-11-03 00:29] wakes up
-[1518-11-04 00:02] Guard #99 begins shift
-[1518-11-04 00:36] falls asleep
-[1518-11-04 00:46] wakes up
-[1518-11-05 00:03] Guard #99 begins shift
-[1518-11-05 00:45] falls asleep
-[1518-11-05 00:55] wakes up
-'''
+import re
+from collections import Counter, defaultdict
 
 
 def parse_line(line):
     # [1518-11-01 00:00] Guard #10 begins shift
     # [1518-11-01 00:05] falls asleep
     # [1518-11-01 00:25] wakes up
-    fields = line.split()
-    # [1518-11-01 00:25]
-    time = datetime.strptime(line[1:line.find(']')], '%Y-%m-%d %H:%M')
-    # Entering shift is awake state
-    state = asleep if fields[2] == 'falls' else awake
-    # #99
-    id = int(fields[3][1:]) if fields[2] == 'Guard' else None
+    match = re.search(r'(\d+)\]', line)
+    assert match, 'cannot find time in {!r}'.format(line)
+    minute = int(match.group(1))
 
-    return Record(time, id, state)
+    match = re.search(r'Guard #(\d+)', line)
+    guard = int(match.group(1)) if match else None
+    state = 'asleep' if 'falls asleep' in line else 'up'
+
+    return minute, guard, state
 
 
 def load_records(fp):
     lines = sorted(fp)  # Sort by time stamp
-    return sorted(parse_line(line) for line in lines)
+    return [parse_line(line) for line in lines]
 
 
 def part_1(records):
     """We assume records are sorted by date/time"""
-    sleeps = defaultdict(Counter)  # id -> minutes asleep
-    curr_id = None
-    for i, record in enumerate(records):
-        prev = records[i-1] if i > 0 else None
-        if record.id:
-            curr_id = record.id
-        if record.state == awake and prev and prev.state == asleep:
-            minutes = range(prev.time.minute, record.time.minute)
-            sleeps[curr_id].update(minutes)
+    sleeps = defaultdict(Counter)  # id -> (minute -> count)
+    sleep_minute = None
+    curr_guard = None
+    for (minute, guard, state) in records:
+        if guard:
+            curr_guard = guard
+            sleep_minute = None
+            continue
 
-    def key(id):
-        return sum(sleeps[key].values())
+        if state == 'asleep':
+            sleep_minute = minute
+            continue
 
-    id = sorted(sleeps, key=key)[0]
-    (minute, _), = sleeps[id].most_common(1)
-    return id * minute
+        if state == 'up' and sleep_minute:
+            sleeps[curr_guard].update(range(sleep_minute, minute))
+            sleep_minute = None
+
+    def key(guard):
+        return sum(sleeps[guard].values())
+
+    guard = max(sleeps, key=key)
+    (minute, _), = sleeps[guard].most_common(1)
+    return guard * minute
 
 
-# from io import StringIO
-# records = load_records(StringIO(example))
-with open('day-4.txt') as fp:
+# file_name = 'day-4-example.txt'
+file_name = 'day-4.txt'
+with open(file_name) as fp:
     records = load_records(fp)
 print(part_1(records))
