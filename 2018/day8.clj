01b89992346c080a4b09bc1851c787cd5696bc3a
Miki Tebeka <miki.tebeka@gmail.com>
2018-12-08 12:57:08 +0200

    a bit nicer code

--- 2018/day8.clj
+++ 2018/day8.clj
@@ -1,20 +1,18 @@
-(def test-data "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
-
 (defn parse-input [input]
   (map #(Integer/parseInt %1) (re-seq #"\d+" input)))
 
-(defn parse-tree
+(defn parse-partial-tree
   "Parses nums into a tree, return a vector of [tree rest-of-nums]"
   [nums]
   (let [num-child (first nums) num-meta (second nums)]
     (loop [nums (nthrest nums 2) n num-child children []]
       (if (zero? n)
         [{:children children :meta (take num-meta nums)} (nthrest nums num-meta)]
-        (let [[child rest-of-nums] (parse-tree nums)]
+        (let [[child rest-of-nums] (parse-partial-tree nums)]
           (recur rest-of-nums (dec n) (conj children child)))))))
 
-;(def nums (parse-input test-data))
-(def nums (parse-input (slurp "day-8.txt")))
+(defn parse-tree [nums]
+  (first (parse-partial-tree nums)))
 
 (defn sum-meta [tree]
   (apply + (concat (tree :meta) (map sum-meta (tree :children)))))
@@ -25,7 +23,8 @@
     (let [children (tree :children)]
       (apply + (map tree-value (map #(nth children (dec %1) nil) (tree :meta)))))))
 
-
-(def tree (first (parse-tree nums)))
+; (def nums (parse-input "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"))
+(def nums (parse-input (slurp "day-8.txt")))
+(def tree (parse-tree nums))
 (println (sum-meta tree))
 (println (tree-value tree))
